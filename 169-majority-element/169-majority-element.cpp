b7dfcac070d67f9f649c0ca0f904c6000c14b8c1
Lavish garg <74075404+lavish002@users.noreply.github.com>
2022-05-03 00:59:38 +0530

    Time: 24 ms (59.23%), Space: 19.6 MB (60.56%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count =0;
+        int elem = 0;
+        
+        for(int num:nums){
+            if(count==0)
+                elem=num;
+            if(elem==num)
+                count++;
+            else
+                count--;
+        }
+        return elem;
+    }
+};
