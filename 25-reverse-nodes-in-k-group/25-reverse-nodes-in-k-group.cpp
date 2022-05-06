367a3e574654e2a33870f19dc6e08d651eea0908
Lavish garg <74075404+lavish002@users.noreply.github.com>
2022-05-06 23:54:38 +0530

    Time: 16 ms (79.74%), Space: 11.5 MB (32.80%) - LeetHub

---
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+class Solution {
+public:
+   
+//utility function to insert ListNode in the list
+void insertNode(ListNode* &head,int val) {
+    ListNode* newNode = new ListNode(val);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    
+    ListNode* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    
+    temp->next = newNode;
+    return;
+}
+//utility function to find length of the list
+int lengthOfLinkedList(ListNode* head) {
+    int length = 0;
+    while(head != NULL) {
+        ++length;
+        head = head->next;
+    }
+    return length;
+}
+//utility function to reverse k nodes in the list
+
+ListNode* reverseKGroup(ListNode* head,int k) {
+    if(head == NULL||head->next == NULL) return head;
+    
+    int length = lengthOfLinkedList(head);
+    
+    ListNode* dummyHead = new ListNode(0);
+    dummyHead->next = head;
+    
+    ListNode* pre = dummyHead;
+    ListNode* cur;
+    ListNode* nex;
+    
+    while(length >= k) {
+        cur = pre->next;
+        nex = cur->next;
+        for(int i=1;i<k;i++) {
+            cur->next = nex->next;
+            nex->next = pre->next;
+            pre->next = nex;
+            nex = cur->next;
+        }
+        pre = cur;
+        length -= k;
+    }
+    return dummyHead->next;
+}
+};
+
+
