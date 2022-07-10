f03fa1574f51afd7f29429ea31c134552f65ea77
Lavish garg <74075404+lavish002@users.noreply.github.com>
2022-07-10 18:25:01 +0530

    Time: 73 ms (15.46%), Space: 6.4 MB (50.50%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+    
+    bool solve(vector<vector<char>>& board){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                
+                if(board[i][j]=='.'){
+                    
+                    for(char c='1'; c<='9'; c++){
+                        if(isValid(board, i, j, c)){
+                            board[i][j]=c;
+                            if(solve(board)){
+                                return true;
+                            }else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool isValid(vector<vector<char>>& board, int i, int j, char c){
+        for(int k=0; k<9; k++){
+            if(board[i][k]==c) return false;
+            if(board[k][j]==c) return false;
+            
+            if(board[(i/3)*3 + k/3][(j/3)*3 + k%3] == c) return false; 
+        }
+        return true;
+    }
+};
