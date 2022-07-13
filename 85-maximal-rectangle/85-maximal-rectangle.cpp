6cc877f6d4d64966ec7996aa0bfc3884131c9869
Lavish garg <74075404+lavish002@users.noreply.github.com>
2022-07-13 11:33:22 +0530

    Time: 70 ms (57.23%), Space: 13.1 MB (80.49%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -48,7 +48,7 @@ public:
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j]!='0'){
-                    temp[j] = temp[j] + 1;
+                    temp[j] = temp[j] + (matrix[i][j]-'0'); // temp[j] = temp[j]+1;
                 }else
                     temp[j] = 0;
             }
