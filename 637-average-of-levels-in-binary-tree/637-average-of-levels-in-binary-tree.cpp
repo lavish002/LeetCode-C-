fed0b8b198fc74537e5ed1a4093e55773986b3ec
Lavish garg <74075404+lavish002@users.noreply.github.com>
2022-09-03 04:32:48 +0530

    Time: 21 ms (68.97%), Space: 22.3 MB (92.56%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -1,32 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        queue<TreeNode*> q;
-        vector<double> res;
-        double temp;
-        int size, i;
-        TreeNode* curr;
-        
-        q.push(root);
-        
-        while (!q.empty()) {
-            temp = 0;
-            size = q.size();
-            
-            for (i = 0; i < size; i++) {
+        queue<TreeNode*> pending;
+        pending.push(root);
+        vector<double> ans = {(double)root->val};
+        while(!pending.empty()){
+            int n = pending.size();
+            double count=0, temp=0;
+            for(int i=0; i<n; i++){
+                TreeNode* front = pending.front();
+                pending.pop();
                 
-                curr = q.front();
-                q.pop();
-                
-                if (curr->left) q.push(curr->left);
-                if (curr->right) q.push(curr->right);
-                
-                temp += curr->val;
+                if(front->right!=NULL){
+                    temp += front->right->val;
+                    pending.push(front->right);
+                    count++;
+                }
+                if(front->left!=NULL){
+                    temp += front->left->val;
+                    pending.push(front->left);
+                    count++;
+                }
             }
-            
-            res.push_back((double)temp / size);
+            if(count!=0) ans.push_back(temp/count);
         }
-        
-        return res;
+        return ans;
     }
 };
